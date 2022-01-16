d7e779c5991f5ee2c80b58058b8c6cc41cdd1419
Jayakrishna Gandhamalla <gandhamallajayakrishna@hotmail.com>
2022-01-16 16:36:52 +0530

    fx Show router-outlet issue fix Added breakpoint listener

--- ui/src/app/core/shell/shell.component.ts
+++ ui/src/app/core/shell/shell.component.ts
@@ -1,8 +1,23 @@
-import { Component } from '@angular/core';
+import { BreakpointObserver, BreakpointState } from '@angular/cdk/layout';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss'],
 })
-export class ShellComponent {}
+export class ShellComponent implements OnInit {
+  smallScreen = new BehaviorSubject<boolean>(false);
+  constructor(public breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    this.breakpointObserver.observe(['(min-width: 768px)']).subscribe((state: BreakpointState) => {
+      if (state.matches) {
+        this.smallScreen.next(false);
+      } else {
+        this.smallScreen.next(true);
+      }
+    });
+  }
+}
